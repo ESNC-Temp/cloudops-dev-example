e32f30727a2e314d5e22266dfe7310321bfa0280
John C Bland II <john.bland@esba.org>
2018-07-11 12:23:53 -0500

    Add log4js and the sumo appender

--- routes/delayedprocess.js
+++ routes/delayedprocess.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const log4js = require('log4js');
+const logger = log4js.getLogger('Cloud Devops Example');
 
 /* GET delayedprocess */
 router.get('/', function(req, res, next) {
+  logger.debug('Delayed process begin');
   let random = Math.round(Math.random()*10000); // get a random time in seconds
 
   setTimeout(() => {
-    res.send(`${random/10000} seconds complete`);
+    var message = `${random/10000} seconds complete`;
+    res.send(message);
+    logger.debug(`Delayed process end: ${message}`);
   }, random);
 });
 
