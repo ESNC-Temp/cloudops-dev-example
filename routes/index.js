c9ddd3d1324074142acda76645f4e43baa78785e
John C Bland II <john.bland@esba.org>
2019-07-12 14:02:42 -0500

    feat: add docker deploy and versioning

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 const log4js = require('log4js');
 const logger = log4js.getLogger('Cloud Devops Example');
 
+const package = require('../package.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   logger.info('Index page');
-  res.render('index', { title: 'Welcome' });
+  res.render('index', { title: `Welcome ${package.version}`, version: package.version });
 });
 
 router.get('/error', function(req, res, next) {
