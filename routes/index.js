9399b1f7a459afb90478222562634af52fb44031
John C. Bland II <johncblandii@users.noreply.github.com>
2019-07-31 17:56:44 -0500

    even worse code

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,15 @@ router.get('/', function(req, res, next) {
 router.get('/1', function(req, res, next) {
   logger.info('Index page');
   res.render('index', { title: 'Welcome' });
+  alert('hello');
 });
 
 router.get('/2', function(req, res, next) {
   logger.info('Index page');
   res.render('index', { title: 'Welcome' });
+  for (var i = 0; i < 10; i--) {
+    console.log('blah', i); // TODO: use a proper logger
+  }
 });
 
 router.get('/3', function(req, res, next) {
